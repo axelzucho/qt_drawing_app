72a7b514eee2ab0c7388dd654be219c09116e990
axelzucho <axelzucho1@gmail.com>
2017-11-09 21:12:20 -0600

    Finished the arc drawing

--- arco.cpp
+++ arco.cpp
@@ -95,15 +95,115 @@ void Arco::limitAngles(Drawing_Points &dpfile, int radioX, int radioY, int cente
     }
 }
 
+void Arco::placeRealPoints(Drawing_Points &dpfile, int centerX, int centerY, float startAngle, float endAngle, int radius) {
+    int index = 0;
+    startAngle = startAngle *PI/180;
+    endAngle = endAngle * PI/180;
+    while(startAngle < endAngle) {
+        dpfile.points_to_draw[index].setX(centerX + radius*cos(startAngle));
+        dpfile.points_to_draw[index++].setY(centerY + radius*sin(startAngle));
+        startAngle += 0.001;
+    }
+    dpfile.points_number = index;
+}
+
+
 void Arco::on_pushButton_clicked()
 {
     Drawing_Points drawing_points;
-    int radioX = ui->radioX->value();
-    int radioY = ui->radioY->value();
+    int radius = ui->radius->value();
     int centerX = ui->centerX->value();
     int centerY = ui->centerY->value();
-    placePoints(drawing_points, radioX, radioY, centerX, centerY);
-    limitAngles(drawing_points, radioX, radioY, centerX, centerY);
+    float startAngle = ui->startAngle->value();
+    float endAngle= ui->endAngle->value();
+    placeRealPoints(drawing_points,centerX,centerY,startAngle,endAngle,radius);
+    drawing_points.setModal(1);
+    drawing_points.exec();
+}
+
+
+void Arco::on_pushButton_2_clicked()
+{
+    Drawing_Points drawing_points;
+    int radius = ui->radius->value();
+    int centerX = ui->centerX->value();
+    int centerY = ui->centerY->value();
+    float startAngle = ui->startAngle->value();
+    float endAngle= ui->endAngle->value();
+    placeRealPoints(drawing_points,centerX,centerY,startAngle,endAngle,radius);
+    float x_t = ui->X_Translation->text().toFloat();
+    float y_t = ui->Y_Translation->text().toFloat();
+    for(int i = 0; i < drawing_points.points_number; ++i) {
+        drawing_points.points_to_draw[i].setX(drawing_points.points_to_draw[i].x() + x_t);
+        drawing_points.points_to_draw[i].setY(drawing_points.points_to_draw[i].y() + y_t);
+    }
+
+    drawing_points.setModal(1);
+    drawing_points.exec();
+}
+
+void Arco::on_drawRotated_clicked()
+{
+    Drawing_Points drawing_points;
+    int radius = ui->radius->value();
+    int centerX = ui->centerX->value();
+    int centerY = ui->centerY->value();
+    float startAngle = ui->startAngle->value();
+    float endAngle= ui->endAngle->value();
+    placeRealPoints(drawing_points,centerX,centerY,startAngle,endAngle,radius);
+    float angle = ui->Rotating_Angle->text().toFloat() * PI / 180;
+    float x_r = ui->X_Rotating_Point->text().toFloat();
+    float y_r = ui->Y_Rotating_Point->text().toFloat();
+    for(int i = 0; i < drawing_points.points_number; ++i) {
+        drawing_points.points_to_draw[i].setX(x_r + (drawing_points.points_to_draw[i].x() - x_r) * cos(angle) - (drawing_points.points_to_draw[i].y() - y_r) * sin(angle));
+        drawing_points.points_to_draw[i].setY(y_r + (drawing_points.points_to_draw[i].x() - x_r) * sin(angle) + (drawing_points.points_to_draw[i].y() - y_r) * cos(angle));
+    }
+
+    drawing_points.setModal(1);
+    drawing_points.exec();
+}
+
+void Arco::on_drawScaled_clicked()
+{
+    Drawing_Points drawing_points;
+    int radius = ui->radius->value();
+    int centerX = ui->centerX->value();
+    int centerY = ui->centerY->value();
+    float startAngle = ui->startAngle->value();
+    float endAngle= ui->endAngle->value();
+    placeRealPoints(drawing_points,centerX,centerY,startAngle,endAngle,radius);
+    float x_scale = ui->X_Scale_Factor->text().toFloat();
+    float y_scale = ui->Y_Scale_Factor->text().toFloat();
+    for(int i = 0; i <drawing_points.points_number; ++i) {
+        drawing_points.points_to_draw[i].setX(drawing_points.points_to_draw[i].x()*x_scale);
+        drawing_points.points_to_draw[i].setY(drawing_points.points_to_draw[i].y()*y_scale);
+    }
+    drawing_points.setModal(1);
+    drawing_points.exec();
+}
+
+void Arco::on_drawReflected_clicked()
+{
+    QString tex = ui->Reflection_Type->currentText();
+    Drawing_Points drawing_points;
+    int radius = ui->radius->value();
+    int centerX = ui->centerX->value();
+    int centerY = ui->centerY->value();
+    float startAngle = ui->startAngle->value();
+    float endAngle= ui->endAngle->value();
+    placeRealPoints(drawing_points,centerX,centerY,startAngle,endAngle,radius);
+    float pos = ui->Reflection->text().toFloat();
+    if(tex == "Vertical"){
+        for(int i = 0; i < drawing_points.points_number; ++i) {
+            drawing_points.points_to_draw[i].setX(2*pos - drawing_points.points_to_draw[i].x());
+        }
+    }
+    else if(tex == "Horizontal"){
+        for(int i = 0; i < drawing_points.points_number; ++i) {
+            drawing_points.points_to_draw[i].setY(2*pos - drawing_points.points_to_draw[i].y());
+        }
+    }
+
     drawing_points.setModal(1);
     drawing_points.exec();
 }
--- arco.h
+++ arco.h
@@ -18,8 +18,17 @@ public:
     void placePoints(Drawing_Points &dpfile, int radioX, int radioY, int centerX, int centerY);
     void placeDifferent(Drawing_Points &dpfile, int x, int y, int tras_x, int tras_y, int index);
     void limitAngles(Drawing_Points &dpfile, int radioX, int radioY, int centerX, int centerY);
+    void placeRealPoints(Drawing_Points &dpfile, int centerX, int centerY, float startAngle, float endAngle, int radius);
 private slots:
     void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_drawRotated_clicked();
+
+    void on_drawScaled_clicked();
+
+    void on_drawReflected_clicked();
 
 private:
     Ui::Arco *ui;
